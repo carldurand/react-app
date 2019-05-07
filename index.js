bd633b02840db01c42e1f51bf4deb1452eb9a1f8
Carl Durand <carldurand.student@careerdevs.com>
2019-05-07 14:21:18 +0000

    Remove Whitespace from Start and End

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-let user = {
-  name: 'Kenneth',
-  age: 28,
-  data: {
-    username: 'kennethCodesAllDay',
-    joinDate: 'March 26, 2016',
-    organization: 'freeCodeCamp',
-    friends: [
-      'Sam',
-      'Kira',
-      'Tomo'
-    ],
-    location: {
-      city: 'San Francisco',
-      state: 'CA',
-      country: 'USA'
-    }
-  }
-};
-
-function addFriend(userObj, friend) {
-  // change code below this line  
-  userObj.data.friends.push(friend);
-  return userObj.data.friends;
-  // change code above this line
-}
-
-console.log(addFriend(user, 'Pete'));
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ''); // Change this line

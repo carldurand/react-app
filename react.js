a7d91a2ae542c3b513410def3bf68d8516fd62d4
Carl Durand <carldurand.student@careerdevs.com>
2019-05-11 01:21:52 +0000

    Use State to Toggle an Element

--- react.js
+++ react.js
@@ -2,23 +2,38 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Initial State'
+      visibility: false
     };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
     // change code below this line
-    this.setState({
-      name: 'React Rocks!'
-    });
+    this.toggleVisibility = this.toggleVisibility.bind(this);
     // change code above this line
   }
+  // change code below this line
+  toggleVisibility() {
+    if (this.state.visibility == true) {
+    this.setState({
+      visibility: false
+    });} else {
+      this.setState({
+        visibility: true
+      })
+    }
+  }
+  // change code above this line
   render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Click Me</button>
-        <h1>{this.state.name}</h1>
-      </div>
-    );
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
   }
 };

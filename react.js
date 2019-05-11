e9cca12ab91401014cf7223cbab4fa213dafb328
Carl Durand <carldurand.student@careerdevs.com>
2019-05-11 14:22:14 +0000

    react app

--- react.js
+++ react.js
@@ -1,39 +1,67 @@
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibility: false
-    };
-    // change code below this line
-    this.toggleVisibility = this.toggleVisibility.bind(this);
-    // change code above this line
+import React from 'react';
+
+class App extends React.Component {
+  state={
+  name:"",
+  email:"",
+  phoneNumber:"",
+  submited: false 
   }
-  // change code below this line
-  toggleVisibility() {
-    if (this.state.visibility == true) {
+  
+  handleSubmit = () => {
+    this.setState({submitted:true})
+  }
+  
+  setName = event  => {
+    console.log(event.target.value);
     this.setState({
-      visibility: false
-    });} else {
-      this.setState({
-        visibility: true
-      })
-    }
+      name:event.target.value
+    });
   }
-  // change code above this line
-  render() {
-    if (this.state.visibility) {
-      return (
-        <div>
-          <button onClick={this.toggleVisibility}>Click Me</button>
-          <h1>Now you see me!</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={this.toggleVisibility}>Click Me</button>
-        </div>
-      );
-    }
+  
+   setEmail = event  => {
+    console.log(event.target.value);
+    this.setState({
+      email:event.target.value
+    });
   }
-};
+  
+  setPhoneNumber = event  => {
+    console.log(event.target.value);
+    this.setState({
+      phoneNumber:event.target.value
+    });
+  }
+  
+  render(){
+  return (
+    <div className="App">
+      <h2>Social profile</h2>
+    <p>An app for your social media profiles</p>
+    { !this.state.submitted &&
+    <div>
+    <input onChange={this.setName} style={{"display":"block"}} placeholder="Name" />
+    <input onChange={this.setEmail} style={{"display":"block"}} placeholder="email" />
+    <input onChange={this.setPhoneNumber} style={{"display":"block"}} placeholder="phone number" />
+    <button onClick={this.handleSubmit }>Submit</button>
+    </div>}
+    
+    {this.state.submitted && 
+    
+    <div>
+    {"Your name is: " + this.state.name}
+    <br />
+    {"your email is: " + this.state.email}
+    <br />
+    {"your phone number is: " + this.state.phoneNumber}
+    
+    </div>}
+    
+    
+    </div>
+  );
+  }
+}
+
+
+export default App;
